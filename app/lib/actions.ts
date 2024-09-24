a12c18be29cdccb24739179f179114cd4645b89a
jbeeze <jared.billig@gmail.com>
2024-09-24 19:40:38 -0400

    error handling;

---
+++ app/lib/actions.ts
@@ -0,0 +1,107 @@
+'use server'
+import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+
+// Creates a schema using the zod library to help in validation and casting of formdata
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string({
+      invalid_type_error: 'Please select a customer',
+    }),
+    // coerce validates and casts to a number
+    amount: z.coerce
+      .number()
+      .gt(0, { message: 'Please enter an amount greater than $0.'}),
+    status: z.enum(['pending', 'paid'], {
+      invalid_type_error: 'Please select an invoice status.',
+    }),
+    date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
+export async function createInvoice(prevState: State, formData: FormData) {
+    const validatedFields = CreateInvoice.safeParse({
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    });
+
+    // If form validation fails, return errors early. Otherwise, continue.
+    console.log(`Validated Fileds: ${validatedFields}`);
+
+    if (!validatedFields.success) {
+      return {
+        errors: validatedFields.error.flatten().fieldErrors,
+        message: 'Missing Fields. Failed to Create Invoice.',
+      };
+    }
+      
+    // Storing monetary amounts in cents to 
+    // eliminate JavaScript floating-point errors and ensure greater accuracy.
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
+   
+    try {
+      await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+      `;
+    } catch (error) {
+      return {
+        message: `Database Error: ${error} // Failed to Create Invoice.`,
+      };
+    }
+   
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+  }
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = UpdateInvoice.parse({
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    });
+   
+    const amountInCents = amount * 100;
+   try {
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+   } catch (error) {
+    return { messsage: `Database Error: ${error} // Failed to Update Invoice` }
+   }
+
+   
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+  }
+
+
+  export async function deleteInvoice(id: string) {
+    //throw new Error("delete, i can not");
+    try {
+      await sql`DELETE FROM invoices WHERE id = ${id}`;
+      revalidatePath('/dashboard/invoices');
+      return { message: 'Deleted Invoice.' };
+    } catch (error) {
+      return { message: `Database Error: ${error} // Failed to Delete Invoice.` };
+    }
+}
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,6 +2,7 @@
  
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
  
 export default function Search({
   placeholder,
@@ -12,9 +13,14 @@ export default function Search({
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
+  // debounced only runs the search after the 300ms of pausing of input (near end)
+  const handleSearch = useDebouncedCallback((term) => {
+    console.log(`Searching... ${term}`);
     // Automatically creates a proper params string to append to URL (e.g ?page=1&query=a)
     const params = new URLSearchParams(searchParams);
+
+    //below resets page back to 1 upon new search
+    params.set('page', '1');
     
     if (term) {
       params.set('query', term);
@@ -24,8 +30,8 @@ export default function Search({
 
     // Uses replace method from useRouter to take the full path name and then 
     // append the params string after the ?
-    replace(`${pathname}?${params.toString()}`)
-  }
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
  
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -36,7 +42,9 @@ export default function Search({
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
         defaultValue={searchParams.get('query')?.toString()}
-        onChange={(e) => handleSearch(e.target.value)}
+        onChange={(e) => {
+          handleSearch(e.target.value)
+        }}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
